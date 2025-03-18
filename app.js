8eb80f2419b892a709e791b2bd36a55c0f3d7d81
valentim <pedamastim@gmail.com>
2025-03-18 01:41:39 -0300

    sortear amigo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+let lista = document.getElementById("listaAmigos");
 
 function adicionarAmigo(){
     let amigo = document.getElementById("amigo");
@@ -14,12 +15,23 @@ function adicionarAmigo(){
 };
 
 function atualizarLista(){
-    let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
     for (let i = 0; i <amigos.length; i++){
         let item = document.createElement("li");
-        item.textContent =amigos [i];
+        item.textContent = amigos [i];
 
         lista.appendChild(item);
     }
 };
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("Insira amigos para sortear!");
+    } else {
+        let indice = Math.floor(Math.random() * amigos.length);
+        let sorteado = amigos[indice];
+        let resultado = document.getElementById("resultado");
+        lista.innerHTML = "";
+        resultado.innerHTML = `O amigo sorteado é: ${sorteado}`;
+    }
+}
